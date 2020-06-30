b48d263a4561e80eecb3d1b269d48cbf406b6018
marp-science <marp.science@protonmail.ch>
2020-06-30 21:45:15 +0200

    added the tool function filters.py for xor filter needs

--- src/credo_cf/commons/__init__.py
+++ src/credo_cf/commons/__init__.py
@@ -1,2 +1,3 @@
 from credo_cf.commons.grouping import group_by_lambda, get_and_set, group_by_device_id, group_by_resolution, group_by_timestamp_division, get_resolution_key
 from credo_cf.commons.consts import *
+from credo_cf.commons.filters import *
