48743a8815152f072e55392058074739ae594f4c
slawekstu <slawekstu@gmail.com>
2020-07-01 13:13:50 +0200

    Aktualizacja dokumentacji - nie wiem czy jest dobrze opisana, sprawdziszz?

--- src/credo_cf/classification/artifact/too_bright.py
+++ src/credo_cf/classification/artifact/too_bright.py
@@ -4,10 +4,37 @@ from credo_cf.commons.consts import BRIGHT_PIXELS,GOOD_BRIGHT, IMAGE
 from credo_cf.image.image_utils import measure_darkness_brightest
 
 """
-too_bright(detections,number,time)
-detections        -   dict with good time detection in one device
-bright_pixels     -   maximum number of bright pixels on the slice
-threshold         -   the bright pixel has a brightness greater than the threshold (range 0 - 255)
+    Analysis of the detection slice by checking the brightness of the pixels
+
+    :param detections: list of detections
+    :param bright_pixels (int) : maximum number of bright pixels on the slice (wycinek)
+    :paramthreshold (int) – value of pixels for the bright_pixel (i.e bright pixel is when pixel have value >70)
+
+
+    The code from "frame_content" is downloaded and saved as a disk image, loaded by functions from the "Image" library 
+    and converted to gray scale LA (convert 'LA') - (8-bit pixels, black and white) with ALPHA. 
+    After conversion, we analyze pixel by pixel counting bright pixels according to the given threshold, 
+    at the end we count how many bright pixels were checking for more than the number of bright pixels, 
+    if we classify more as an artifact.
+
+
+    Required keys:
+      * ``frame_content``: base64 image our detection
+
+    Keys will be add:
+      * ``BRIGHT_PIXELS``: number of bright pixels
+      * ``GOOD_BRIGH``: True,or False.
+
+    gray change used:
+    https://pillow.readthedocs.io/en/stable/reference/Image.html
+
+    Example::
+
+      good,bad =  bright_detections(detections,70,70)
+
+    :return: tuple of (list of good detections, list of bad detections)
+    Return type
+    Tuple[List[dict], List[dict]]
 """
 
 def bright_detections(detections: List[dict], bright_pixels=70, threshold=70) -> Tuple[List[dict], List[dict]]:
