9f64ce62c3e92e2783b6f7e3d0ec100cfe14351e
nkg <nkg753@gmail.com>
2020-09-02 10:58:04 +0200

    Export methods

--- src/credo_cf/io/__init__.py
+++ src/credo_cf/io/__init__.py
@@ -1,2 +1,2 @@
 from credo_cf.io.load_write import load_json_from_stream, load_json, serialize, deserialize
-from credo_cf.io.io_utils import progress_and_process_image, progress_load_filter
+from credo_cf.io.io_utils import progress_and_process_image, progress_load_filter, decode_base64, encode_base64, store_png
