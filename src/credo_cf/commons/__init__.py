2c18aaf9e370c0d0ae5a6da319737f226e6ba0e8
nkg <nkg753@gmail.com>
2020-09-11 16:06:14 +0200

    Export print_log function

--- src/credo_cf/commons/__init__.py
+++ src/credo_cf/commons/__init__.py
@@ -1,3 +1,4 @@
 from credo_cf.commons.grouping import group_by_lambda, get_and_set, group_by_device_id, group_by_resolution, group_by_timestamp_division, get_resolution_key
 from credo_cf.commons.consts import *
 from credo_cf.commons.filters import *
+from credo_cf.commons.utils import print_log, get_and_set, get_and_add, join_tuple, get_resolution_key, get_xy_key, point_to_point_distance
