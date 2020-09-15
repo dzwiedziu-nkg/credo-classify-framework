3eeecd9bd65ffb901e1348d260b1f2d28b67f684
nkg <nkg753@gmail.com>
2020-09-15 18:51:41 +0200

    Types

--- src/credo_cf/classification/clustering/clustering_utils.py
+++ src/credo_cf/classification/clustering/clustering_utils.py
@@ -3,7 +3,7 @@ from typing import List
 import numpy as np
 
 
-def make_stack(list_of_matrices: List[np.ndarray]):
+def make_stack(list_of_matrices: List[np.ndarray]) -> np.ndarray:
     """
     Join array of xD matrices to one (x+1)D matrix. I.e. list od 2D matrix with shape (a,b) join to 3D matrix with shape (len(list), a, b)
     :param list_of_matrices: list of matrices to join
