1d67b165f6ee0b3b7e7d44ebc8597aa82ee41bd6
nkg <nkg753@gmail.com>
2020-10-02 14:36:23 +0200

    Changes: turn off JSON generation, only JSON's

--- example/experiments/05_make_simple_classified_sets.py
+++ example/experiments/05_make_simple_classified_sets.py
@@ -23,6 +23,9 @@ def main():
     spots = []
     tracks = []
     worms = []
+    others = []
+    all = []
+    multi = []
 
     for d in objs:
         astropy_measures(d)
@@ -30,23 +33,38 @@ def main():
         d.pop(FRAME_DECODED)
 
         if d[ASTROPY_FOUND] == 1:
+            all.append(d)
+
             if d[ASTROPY_ELLIPTICITY][0] < 0.1 and d[ASTROPY_SOLIDITY][0] > 0.8:
                 spots.append(d)
             elif d[ASTROPY_ELLIPTICITY][0] > 0.8 and d[ASTROPY_SOLIDITY][0] > 0.8:
                 tracks.append(d)
             elif d[ASTROPY_SOLIDITY][0] < 0.8:
                 worms.append(d)
+            else:
+                others.append(d)
+        elif d[ASTROPY_FOUND] > 1:
+            multi.append(d)
 
     def store_pngs(arr, subdir):
         for a in arr:
-            store_png(OUTPUT_DIR, [subdir], '%.3f_%d' % (a[ASTROPY_SOLIDITY][0], a[ID]), a[IMAGE])
-            a.pop(IMAGE)
-        with open(os.path.join(OUTPUT_DIR, '%s.json' % subdir), 'w') as json_file:
-            json.dump({'detections': arr}, json_file)
+            store_png(OUTPUT_DIR, [subdir, 'by_solidity'], '%.3f_%.3f_%d' % (a[ASTROPY_SOLIDITY][0], a[ASTROPY_ELLIPTICITY][0], a[ID]), a[IMAGE])
+            store_png(OUTPUT_DIR, [subdir, 'by_ellipticity'], '%.3f_%.3f_%d' % (a[ASTROPY_ELLIPTICITY][0], a[ASTROPY_SOLIDITY][0], a[ID]), a[IMAGE])
+
+            s = int(a[ASTROPY_SOLIDITY][0] * 5) * 2
+            e = int(a[ASTROPY_ELLIPTICITY][0] * 5) * 2
+            store_png(OUTPUT_DIR, [subdir, 'by_matrix_solidity_per_ellipticity', '%02d-%02d_%02d-%02d' % (s, s + 2, e, e + 2)], '%d' % a[ID], a[IMAGE])
+
+            # a.pop(IMAGE)
+        # with open(os.path.join(OUTPUT_DIR, '%s.json' % subdir), 'w') as json_file:
+        #     json.dump({'detections': arr}, json_file)
 
     store_pngs(spots, 'spots')
     store_pngs(tracks, 'tracks')
     store_pngs(worms, 'worms')
+    store_pngs(others, 'others')
+    store_pngs(all, 'all')
+    store_pngs(multi, 'multi')
 
 
 if __name__ == '__main__':
