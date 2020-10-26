69e58a5a2cd0194f13bf8f7b8f6f70e73cf5e91a
nkg <nkg753@gmail.com>
2020-10-26 12:04:45 +0100

    Export save_json_to_stream and save_json

--- src/credo_cf/io/__init__.py
+++ src/credo_cf/io/__init__.py
@@ -1,2 +1,2 @@
-from credo_cf.io.load_write import load_json_from_stream, load_json, serialize, deserialize, deserialize_or_run
+from credo_cf.io.load_write import load_json_from_stream, load_json, save_json_to_stream, save_json, serialize, deserialize, deserialize_or_run
 from credo_cf.io.io_utils import progress_and_process_image, progress_load_filter, decode_base64, encode_base64, store_png
