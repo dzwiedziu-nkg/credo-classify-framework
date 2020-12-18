7ece17a44dd322f1583dc245b26610de671fa315
nkg <nkg753@gmail.com>
2020-12-18 16:09:36 +0100

    Rafactor to methods

--- example/hit_path.py
+++ example/hit_path.py
@@ -1,10 +1,13 @@
 import math
 
 from scipy.interpolate import interp2d
-from scipy.ndimage import rotate
+from scipy.ndimage import rotate, center_of_mass
 from skimage.feature import canny
-from skimage.morphology import medial_axis, square, erosion
-from skimage.transform import probabilistic_hough_line
+from skimage.filters import rank, gaussian
+from skimage.measure import subdivide_polygon
+from skimage.morphology import medial_axis, square, erosion, disk
+from skimage.segmentation import active_contour
+from skimage.transform import probabilistic_hough_line, rescale
 
 from credo_cf import load_json, progress_and_process_image, group_by_id, GRAY, nkg_mark_hit_area, NKG_MASK, nkg_make_track, NKG_PATH, NKG_DIRECTION, \
     NKG_DERIVATIVE, ID, NKG_THRESHOLD, NKG_UPSCALE, NKG_SKELETON
@@ -29,7 +32,7 @@ used_hits2 = [7741225, 7238971, 5973441, 4892405, 17432760,
              7148947, 4899235, 18349704, 18250739, 6908292,
              9129139, 17771578, 17861029, 17337669, 7470695]
 
-used_hits = used_hits1
+used_hits = used_hits2
 
 hits = []
 for u in used_hits:
@@ -96,6 +99,81 @@ ray_way_octet = [
     ],
 ]
 
+ray_way_octet2 = [
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [1, 1, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0],
+        [1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+    ],
+    [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+    ],
+]
+
+
+fit_mask = [
+    [0, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1],
+    [0, 1, 1, 1, 0],
+]
+
 
 def build_ray_way(octet):
     ret = []
@@ -156,6 +234,100 @@ def in_angle(a, b, v):
         return not (pa >= pv >= pb)
 
 
+def nkg_pather(img, threshold, fov=90, step=1):
+    # mask = np.zeros(img.shape)
+    img = img.copy()
+    start = unravel_index(img.argmax(), img.shape)
+    calc = calc_ways(h['smooth'], start, ray_way)
+    direction = max(calc, key=lambda x: x['value'])
+    next_pos = calc_pos(start, way_next_point(direction['way'], step))
+
+    angle = direction['angle']
+    next_angle = direction['angle'] - 180
+    # img[start] = 0
+    path = []
+
+    while img[next_pos] > threshold:
+        c1 = next_pos
+        path.append(next_pos)
+        # mask[next_pos] = 1
+        img[next_pos] = 0
+
+        try:
+            calc = calc_ways(img, next_pos, ray_way)
+        except:
+            # edge achieved
+            break
+
+        filtered = list(filter(lambda x: in_angle(angle - fov / 2, angle + fov / 2, x['angle']), calc))
+        direction = max(filtered, key=lambda x: x['value'])
+        next_pos = calc_pos(next_pos, way_next_point(direction['way'], step))
+        angle = direction['angle']
+
+    path2 = []
+    angle = next_angle
+    next_pos = start
+    while img[next_pos] > threshold:
+        path2.append(next_pos)
+        c2 = next_pos
+        # mask[next_pos] = 1
+        img[next_pos] = 0
+
+        try:
+            calc = calc_ways(img, next_pos, ray_way)
+        except:
+            # edge achieved
+            break
+
+        filtered = list(filter(lambda x: in_angle(angle - fov / 2, angle + fov / 2, x['angle']), calc))
+        direction = max(filtered, key=lambda x: x['value'])
+        next_pos = calc_pos(next_pos, way_next_point(direction['way'], step))
+        angle = direction['angle']
+
+    return np.array([*reversed(path2), *path])
+
+
+def line_to_mask(img, path, scale=1, value=1, create_new_mask=False):
+    if create_new_mask:
+        mask = np.zeros(img.shape)
+    else:
+        mask = img
+
+    for a in path:
+        if scale > 1:
+            mask[round(a[0] * scale + scale/2.0), round(a[1] * scale + scale/2.0)] = value
+        else:
+            mask[round(a[0]), round(a[1])] = value
+
+    return ma.masked_array(img, mask)
+
+
+def path_to_center_of_weight(img, fm, path):
+    path2 = []
+    fit_mask = np.array(fm)
+    w = fit_mask.shape[0]
+    h = fit_mask.shape[1]
+
+    fit = img.copy()
+    for i in path:
+        x1 = int(i[0] - (w - 1) / 2)
+        x2 = int(i[0] + (w + 1) / 2)
+        y1 = int(i[1] - (h - 1) / 2)
+        y2 = int(i[1] + (h + 1) / 2)
+        # cut = fit[i[0]-2:i[0]+3, i[1]-2:i[1]+3]
+        cut = fit[x1:x2, y1:y2]
+        if cut.shape[0] != 5 or cut.shape[1] != 5:
+            continue
+        m = cut * fit_mask
+
+        new_i = center_of_mass(m)
+        path2.append([new_i[0] + x1, new_i[1] + y1])
+
+    path2 = np.array(path2)
+
+    return subdivide_polygon(path2)
+
+
 ray_way = build_ray_way(ray_way_octet)
 www = way_next_point(ray_way[0]['way'])
 
@@ -166,59 +338,30 @@ for h in hits:
     nkg_mark_hit_area(h)
 display_all_from(hits, NKG_MASK)
 
+for h in hits:
+    img = h.get(GRAY).copy()
+
+    img = gaussian(img, 0.5)  # rank.mean(img, selem=disk(1))
+    h['smooth'] = img
+
+
+display_all_from(hits, 'smooth')
+
 
 for h in hits:
-    step = 1
-    fov = 90
 
-    img = h.get(GRAY).copy()
+    path = nkg_pather(h.get(GRAY), h.get(NKG_THRESHOLD))
+    h['masked'] = line_to_mask(h.get(GRAY), path, create_new_mask=True)
+
+    snake = path_to_center_of_weight(h.get(GRAY), fit_mask, path)
+
+    # snake = active_contour(masked, init, boundary_condition='fixed',
+    #                       alpha=0.1, beta=1.0, w_line=-5, w_edge=0, gamma=0.1)
+
+    img = rescale(h.get(GRAY), 8, order=0, preserve_range=True, anti_aliasing=False)
     mask = np.zeros(img.shape)
-    start = unravel_index(img.argmax(), img.shape)
-    calc = calc_ways(img, start, ray_way)
-    direction = max(calc, key=lambda x:x['value'])
-    next_pos = calc_pos(start, way_next_point(direction['way'], step))
+    h['masked2'] = line_to_mask(img, snake, scale=8, create_new_mask=True)
 
-    angle = direction['angle']
-    next_angle = direction['angle'] - 180
-    # img[start] = 0
-
-    while img[next_pos] > h[NKG_THRESHOLD]:
-        mask[next_pos] = 1
-        img[next_pos] = 0
-
-        try:
-            calc = calc_ways(img, next_pos, ray_way)
-        except:
-            # edge achieved
-            break
-
-        filtered = list(filter(lambda x: in_angle(angle - fov / 2, angle + fov / 2, x['angle']), calc))
-        direction = max(filtered, key=lambda x: x['value'])
-        next_pos = calc_pos(next_pos, way_next_point(direction['way'], step))
-        angle = direction['angle']
-
-    angle = next_angle
-    next_pos = start
-    while img[next_pos] > h[NKG_THRESHOLD]:
-        mask[next_pos] = 1
-        img[next_pos] = 0
-
-        try:
-            calc = calc_ways(img, next_pos, ray_way)
-        except:
-            # edge achieved
-            break
-
-        filtered = list(filter(lambda x: in_angle(angle - fov / 2, angle + fov / 2, x['angle']), calc))
-        direction = max(filtered, key=lambda x: x['value'])
-        next_pos = calc_pos(next_pos, way_next_point(direction['way'], step))
-        angle = direction['angle']
-
-    masked = ma.masked_array(h.get(GRAY), mask=mask)
-    h['masked'] = masked
-
-    # edges = canny(img, 2, 1, 25)
-    # h['edges'] = edges
-    # lines = probabilistic_hough_line(img, threshold=10, line_length=5, line_gap=3)
 
 display_all_from(hits, 'masked')
+display_all_from(hits, 'masked2')
